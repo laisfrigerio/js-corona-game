80970e34c806c5c085b42959e76d4100c21f0fe6
Lais Frigério <laisfrigerio@hotmail.com>
2020-06-28 23:36:27 -0300

    Show scenario
    Signed-off-by: Lais Frigério <laisfrigerio@hotmail.com>

--- src/engine/scenario.js
+++ src/engine/scenario.js
@@ -8,7 +8,7 @@ class Scenario {
 
     draw() {
         image(this.image, this.x2, 0, width, height);
-        image(this.image, this.x1, 0, width, height);
+        image(this.image, this.x1, 0, width - 1, height);
     }
 
     move() {
@@ -16,11 +16,11 @@ class Scenario {
         this.x2 = this.x2 - this.velocity;
 
         if (this.x1 < -width) {
-            this.x1 = width;
+            this.x1 = width - 10;
         }
 
         if (this.x2 < -width) {
-            this.x2 = width;
+            this.x2 = width  - 10;
         }
     }
 }
